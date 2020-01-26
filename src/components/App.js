2e79843c1a17deda46f3fd2c8d7adfb5456d7e7c
Ehl <louishatte@gmail.com>
2020-01-26 06:30:21 +0100

    Create dApp Ethereum Starter Kit

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import './App.css'
+
+import Web3 from 'web3'
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '0x0',
+        }
+    }
+
+    async componentDidMount() {
+        window.ethereum.autoRefreshOnNetworkChange = false
+        await this.loadWeb3()
+        await this.loadAccount()
+    }
+
+    async loadWeb3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    }
+
+    async loadAccount() {
+        const accounts = await window.web3.eth.getAccounts()
+        this.setState({account: accounts[0]})
+    }
+
+    render() {
+        return (
+            <div className="main-div">
+                <h1>Decentralized Application Kit Starter</h1>
+                <h2>Account address: {this.state.account}</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ truffle-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 7545,
+            network_id: '*'
+        }
+    },
+    contracts_directory: './src/contracts/',
+    contracts_build_directory: './src/abis/'
+}
